3b204f38e99a68ec647ac42b941610641a0f95aa
Erwing Candelario <ingeniero_79@hotmail.com>
2023-12-21 20:19:06 -0500

    Eliminacion de algunos comentarios y carpetas vacias

--- src/hook/UseLoader.js
+++ src/hook/UseLoader.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
 
 const UseLoader = ({ isloading = false }) => {
-	// resusable stateful logic
-
 	const [isLoading, setLoading] = useState(isloading);
 
 	return [isLoading, setLoading];
