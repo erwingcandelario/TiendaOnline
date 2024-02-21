c4fd7017ca84cfeb8b408a4d6d3dfa8ec3c96abb
Erwing Candelario <ingeniero_79@hotmail.com>
2024-02-20 23:15:33 -0500

    Busqueda por marca

--- src/components/leftSideBar/LeftSideBar.js
+++ src/components/leftSideBar/LeftSideBar.js
@@ -1,50 +1,21 @@
-import React from "react";
-import "./leftSideBar.css";
-import { Link } from "react-router-dom";
-// import PropTypes from 'prop-types';
+import React from 'react';
+import './leftSideBar.css';
 
-export const LeftSideBar = ({ products }) => {
-     console.log(products);
+export const LeftSideBar = (props) => {
+	console.log('LeftSideBar', props);
 
-    //  if (products === undefined) {
-    //     return <div>Loading...</div>;
-    //   }
-      
-    //  const productsByCompany = products.reduce((acc, product) => {
-    //     acc[product.company] = (acc[product.company] || 0) + 1;
-    //     return acc;
-    //   }, {});
-
-  return (
-    <div className="card left-side-bar">
-      <div className="card-body">
-        <h5 className="card-title">Marca</h5>
-      </div>
-      {/* <ul className="list-group list-group-flush" >
-        {Object.keys(productsByCompany).map((company) => (
-            <li key={company} className="list-group-item" >
-                <Link className="link-item" to={`/products/${company}`} >{company} ({productsByCompany[company]}) </Link>
-            </li>
-        ))}
-      </ul> */}
-      {/* <ul className="list-group list-group-flush">
-        {products?.lenght > 0 ? (
-          products.map((product) => (
-            <li className="list-group-item"> <Link className="link-item" to="/products" >{product?.company} </Link></li>
-          ))
-        ) : (
-          <span>No hay marcas relacionadas</span>
-        )}
-      </ul> */}
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item"> <Link className="link-item" to ="/products/samsung" >Samsung</Link> (3)  </li>
-        <li className="list-group-item"> <Link className="link-item" to ="/products/xiaomi" >Xiaomi</Link> (4) </li>
-        <li className="list-group-item"> <Link className="link-item" to ="/products/apple" >Apple</Link> (1) </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div className="card left-side-bar">
+			<div className="card-body">
+				<h5 className="card-title">Marca</h5>
+			</div>
+			<ul className="list-group list-group-flush">
+				{props?.products?.map((agg) => (
+					<li key={agg.key} onClick={() => props.searchProduct(agg)} className="list-group-item">
+						{agg.key} {agg.count}{' '}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
-
-// LeftSideBar.propTypes = {
-//     company: PropTypes.string.isRequired
-// };
--- src/services/productListService.js
+++ src/services/productListService.js
@@ -1,22 +1,14 @@
 async function getList(
-	name,
-	company,
-	price,
-	description,
-	urlImage,
-	largeDescription,
-	score,
-	aggregate
+	name = '',
+	company = '',
+	price = '',
+	description = '',
+	urlImage = '',
+	largeDescription = '',
+	score = '',
+	aggregate = ''
 ) {
 	try {
-		name = name === undefined ? '' : name;
-		company = company === undefined ? '' : company;
-		price = price === undefined ? '' : price;
-		description = description === undefined ? '' : description;
-		urlImage = urlImage === undefined ? '' : urlImage;
-		largeDescription = largeDescription === undefined ? '' : largeDescription;
-		score = score === undefined ? '' : score;
-		aggregate = aggregate === undefined ? '' : aggregate;
 		const response = await fetch(
 			`http://localhost:8088/products?name=${name}&company=${company}&price=${price}&description=${description}&urlImage=${urlImage}&largeDescription=${largeDescription}&score=${score}&aggregate=${aggregate}`
 		)
@@ -29,6 +21,19 @@ async function getList(
 		throw error;
 	}
 }
+
+async function searchServiceProduct(url = '') {
+	try {
+		const response = await fetch(url)
+			.then((res) => res.json())
+			.then((response) => response);
+		return response;
+	} catch (error) {
+		console.error('Error product =>', error);
+		throw error;
+	}
+}
+
 async function product(id) {
 	try {
 		const response = await fetch('http://localhost:8088/products/' + id)
@@ -77,8 +82,9 @@ async function editProduct(product) {
 			method: 'PUT',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify(product),
-		})	.then((res) => res.json())
-		.then((response) => response);
+		})
+			.then((res) => res.json())
+			.then((response) => response);
 		console.log('editProduct =>', response);
 		return response;
 	} catch (error) {
@@ -87,4 +93,4 @@ async function editProduct(product) {
 	}
 }
 
-export { createProduct, delProduct, getList, product, editProduct };
+export { createProduct, delProduct, editProduct, getList, product, searchServiceProduct };
